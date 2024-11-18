696bebae7eca1c6f5cda0b60ef8269fdfe566787
Itamar Berkowitz <itamar.berkowitz@post.runi.ac.il>
2024-11-18 18:18:35 +0200

    Completed

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,23 @@
 // Prints a crowd cheering output.
+
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args[0].toUpperCase();
+                int numOfTimes = Integer.parseInt(args[1]);
+                for(int i = 0; i < word.length(); i++) {
+                        char c = word.charAt(i);
+                        System.out.print("Give me ");
+                        if ("AEFHILMNORSX".indexOf(c)==-1) {
+                                System.out.print("a ");
+                        }
+                        else {
+                                System.out.print("an ");
+                        }
+                        System.out.println(c + ": " + c + "!");
+                }
+                System.out.println("What does that spell?");
+                for(int i = 0; i < numOfTimes; i++) {
+                        System.out.println(word + "!!!");
+                }
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,23 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int N = Integer.parseInt(args[0]);
+		String mode = args[1];
+		for (int seed=1; seed <= N; seed++) {
+			int steps=1;
+			int currentNum = seed;
+			do {										//in case seed = 1 we will use do while so it will enter the first loop
+				if (mode.equals("v"))
+					System.out.print(currentNum + " ");
+				if (currentNum%2==0)
+					currentNum /= 2;
+				else
+					currentNum = currentNum * 3 + 1;
+				steps++;
+			} while (currentNum != 1);
+			if (mode.equals("v"))
+				System.out.println(currentNum + " (" + steps + ")");		//printing of the last number which is 1 and the steps number
+		}
+		System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
 	}
 }
